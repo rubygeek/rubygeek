dd083fbfce0f5dbb93fb776dfbd0d74a50683c4d
Nola Stowe <mrnicksgirl@gmail.com>
2009-09-09 06:42:30 -0500

    got first spec to pass

--- ruby/hangman_game/lib/hangman/game.rb
+++ ruby/hangman_game/lib/hangman/game.rb
@@ -1,4 +1,11 @@
 module Hangman
   class Game
+    def initialize(messenger)
+      @messenger = messenger
+    end
+    
+    def start
+      @messenger.puts "Welcome to Hangman!"
+    end
   end
 end
--- ruby/hangman_game/spec/hangman/game_spec.rb
+++ ruby/hangman_game/spec/hangman/game_spec.rb
@@ -2,5 +2,13 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module Hangman
   describe Game do
+    context "starting up" do
+      it "should send a welcome message" do
+        messenger = mock("messenger")
+        game = Game.new(messenger)
+        messenger.should_receive(:puts).with("Welcome to Hangman!")
+        game.start
+      end
+    end
   end
 end
