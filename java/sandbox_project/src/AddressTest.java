df6a824a88e30b8cb54bfd42c1073b68a902e763
Nola Stowe <nolastowe@nola.local>
2009-11-22 12:05:25 -0600

    added another test to test the order of the build

--- java/sandbox_project/src/AddressTest.java
+++ java/sandbox_project/src/AddressTest.java
@@ -8,10 +8,19 @@ public class AddressTest extends TestCase {
 		assertEquals("Zip is 99999", "99999", expected.getZip());
 	}
 	
-	public void testBuilderAddressStateZip() {
+	public void testBuilderStreetStateZip() {
 		Address expected = new Address.Builder().street("this is an address").state("YY").zip("11111").build();
 		assertEquals("Address is this is an address", "this is an address", expected.getStreet());
 		assertEquals("Zip is 11111", "11111", expected.getZip());
 		assertEquals("State is YY", "YY", expected.getState());		
 	}
+
+	// Try a different order
+	public void testBuilderZipStreetState() {
+		Address expected = new Address.Builder().zip("11111").street("this is an address").state("YY").build();
+		assertEquals("Address is this is an address", "this is an address", expected.getStreet());
+		assertEquals("Zip is 11111", "11111", expected.getZip());
+		assertEquals("State is YY", "YY", expected.getState());		
+	}
+
 }
