d42c1c2e733069d170499b4e30060770caded10a
Nola Stowe <nolastowe@nola.local>
2010-02-22 14:48:15 -0600

    some more work on django sample app

--- python/trying_django/polls/models.py
+++ python/trying_django/polls/models.py
@@ -1,3 +1,20 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
+    
+    def __unicode__(self):
+        return self.question
+        
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.datetime.today()
+        
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.choice
