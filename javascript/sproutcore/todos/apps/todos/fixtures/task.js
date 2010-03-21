50889dc527c8293207e084d44c6809871b020f24
Nola Stowe <nolastowe@nola.local>
2010-03-21 14:21:37 -0500

    added task model, fixtures and view

--- javascript/sproutcore/todos/apps/todos/fixtures/task.js
+++ javascript/sproutcore/todos/apps/todos/fixtures/task.js
@@ -8,28 +8,19 @@ sc_require('models/task');
 
 Todos.Task.FIXTURES = [
 
-  // TODO: Add your data fixtures here.
-  // All fixture records must have a unique primary key (default 'guid').  See 
-  // the example below.
-
-  // { guid: 1,
-  //   firstName: "Michael",
-  //   lastName: "Scott" },
-  //
-  // { guid: 2,
-  //   firstName: "Dwight",
-  //   lastName: "Schrute" },
-  //
-  // { guid: 3,
-  //   firstName: "Jim",
-  //   lastName: "Halpert" },
-  //
-  // { guid: 4,
-  //   firstName: "Pam",
-  //   lastName: "Beesly" },
-  //
-  // { guid: 5,
-  //   firstName: "Ryan",
-  //   lastName: "Howard" }
+  { "guid" : "task-1",
+    "description" : "Build my first app",
+    "isDone" : false
+  },
+  
+  { "guid" : "task-2",
+    "description" : "Build another app",
+    "isDone" : false 
+  },
+  
+  { "guid" : "task-3",
+    "description" : "Build a third app",
+    "isDone" : false
+  }
 
 ];
