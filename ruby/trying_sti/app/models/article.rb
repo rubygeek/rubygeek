328c7fe13d4aabb78a94723327f53e1d0507c9fe
Nola Stowe <nolastowe@nola.local>
2010-04-22 10:29:16 -0500

    added sti classes

---
+++ ruby/trying_sti/app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+  has_many :documents
+  has_many :images
+  
+  accepts_nested_attributes_for :documents, :allow_destroy => true, :reject_if => lambda {  |attributes|  attributes['filename'].blank?}
+end
