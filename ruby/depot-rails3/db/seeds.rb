a149e7afa7e8bc9ef59b8480b9219a9b31411e06
Nola Stowe <nolastowe@nola.gateway.2wire.net>
2010-07-31 22:39:36 -0500

    work on depot tutorial, added haml and hamlized index. Yay HAML!

--- ruby/depot-rails3/db/seeds.rb
+++ ruby/depot-rails3/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+Product.create(:title => 'Programming Ruby 1.9',
+    :description => 
+        %{ Ruby is the fastest growing and must exciting dynamic
+           language out there. If you need to get working programs
+           delivered fast you should add Ruby to your toolbox.
+         },
+    :image_url => '/images/ruby.jpg',
+    :price => 49.50
+    )
+        
