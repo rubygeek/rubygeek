88da1177e8580840f8e02798bdb2027dabd8d198
Nola Stowe <nolastowe@nola.gateway.2wire.net>
2010-07-31 21:41:26 -0500

    commited new rails3 app with product scaffold

---
+++ ruby/depot-rails3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '6bd6c933cb06ac1209ada98a15d6a33281d68c32edc4ad4159b0ba87954ba616bd0d2f29dcaf21132e4bd77ebc79ae113fcc36b25e5f2df09bb12f05211006c8'
