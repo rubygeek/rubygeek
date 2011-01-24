b04a8e36379885ac1af910755ecf00d3369c0b39
Nola Stowe <nola@rubygeek.com>
2011-01-24 01:07:28 -0600

    added what was next.

--- html5/snake_game/js/snake.js
+++ html5/snake_game/js/snake.js
@@ -11,7 +11,7 @@ var SnakeGame = function(canvas) {
   
   var snakeBits = [],              // position of each bit of snake
       heading,                     // current heading (N S E W)
-      hitsToGrow = FOOD_GROWTH,    // Number of bits left to grow
+      bitsToGrow = FOOD_GROWTH,    // Number of bits left to grow
       timer,                       // Game Loop
       food;                        // current food position
 
@@ -22,9 +22,8 @@ var SnakeGame = function(canvas) {
   function startGame() {
     heading = EAST;
     snakeBits.unshift(bit(4,4));
-    snakeBits.unshift(bit(5, 4));
 
-    //placeFood();
+    placeFood();
    
     clearInterval(timer);
     timer = setInterval(gameLoop, 100); //10 times a second
@@ -39,6 +38,27 @@ var SnakeGame = function(canvas) {
   }
 
   function advanceSnake() {
+    var head = snakeBits[0];
+    switch(heading) {
+      case NORTH:
+        snakeBits.unshift(bit(head.x, head.y-1) );
+        break;
+      case SOUTH:
+        snakeBits.unshift(bit(head.x, head.y+1) );
+        break;
+      case EAST:
+        snakeBits.unshift(bit(head.x + 1, head.y) );
+        break;
+      case WEST:
+        snakeBits.unshift(bit(head.x - 1, head.y) );
+        break;
+    }
+    if (0 == bitsToGrow) {
+      snakeBits.pop();
+    } else {
+      // leave old tip of tail for growth
+      bitsToGrow--;
+    }
   }
 
   function checkCollision() {
@@ -47,12 +67,37 @@ var SnakeGame = function(canvas) {
   function clearCanvas() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
   }
+  
+  function inSnake(x, y, includeHead) {
+    var length = snakeBits.length,
+        i = includeHead ? 0 : 1;
+    for( ; i< length; i++) {
+      if (x == snakeBits[i].x && y == snakeBits[i].y) 
+        return true;
+    }
+    return false;
+  }
+   
+  function placeFood() {
+    var x = Math.round(Math.random() * (MAX_X - 1)),
+        y = Math.round(Math.random() * (MAX_Y - 1));
 
-  function drawSnake() {
-    
+    if (inSnake(x, y, true)) return placeFood();
+
+    food = {x:x, y:y};
   }
 
   function drawFood() {
+    drawInCell(food.x, food.y, function() {
+      ctx.fillStyle = 'orange';
+      ctx.beginPath();
+      ctx.arc(CELL_SIZE/2, CELL_SIZE/2,
+              CELL_SIZE/2, 0, 2*PI, true);
+      ctx.fill();
+     });
+  }
+
+  function drawSnake() {
     var i, length = snakeBits.length;
     for(i = 0; i < length; i++){
       drawBit(snakeBits[i]);
