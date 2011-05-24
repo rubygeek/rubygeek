a87b55d818be6e846ea4e6c28450def7e5ccf435
Nola Stowe <nola@rubygeek.com>
2011-05-24 08:15:42 -0500

    progress working thru peepcode nodejs

---
+++ nodejs/livestats/lib/livestats.js
@@ -0,0 +1,50 @@
+require.paths.unshift(__dirname + "/vendor"); //add vendor to beginning of path
+
+var http = require('http'),
+    sys  = require('sys'),
+    nodeStatic = require('node-static/lib/node-static');
+
+function LiveStats(options) {
+  // if this is called without the new operator it will call new
+  // code from John Resig
+  if (! (this instanceof arguments.callee)) {
+    return new arguments.callee(arguments);
+  }
+  
+  var self = this;
+  
+  self.settings = {
+    port: options.port,
+    geoipServer: {
+      hostname: options.geoipServer.hostname
+      , port: options.geoipServer.port || 80
+    }
+  };
+  
+  self.init();
+
+};
+
+LiveStats.prototype.init = function() {
+  var self = this;
+  self.httpServer = self.createHttpServer();
+  self.httpServer.listen(self.settings.port);
+  sys.log('Server started on port: '+ self.settings.port);
+}
+
+LiveStats.prototype.createHttpServer = function() {
+  var self = this;
+  var server = http.createServer(function(request, response) {
+  	var file = new nodeStatic.Server('./public', { 
+      cache: false
+  	});
+
+  	request.addListener('end', function() {
+  		file.serve(request, response);
+  	});
+  });
+  return server;
+}
+
+// uses commonjs system for building modules
+module.exports = LiveStats;
--- nodejs/livestats/server.js
+++ nodejs/livestats/server.js
@@ -1,22 +1,19 @@
 require.paths.unshift(__dirname + "/vendor"); //add vendor to beginning of path
 
-// module are assigned a local variable in application
-// using commonjs
-// http is one of the built in objects
-// assigning to local variable reduces clutter in global scope
-var http = require('http'),
-    sys  = require('sys'),
-    nodeStatic = require('node-static/lib/node-static');
+process.addListener('uncaugh exception', function (err, stack) {
+  console.log('------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------');  
+});
 
-var server = http.createServer(function(request, response) {
-	var file = new nodeStatic.Server('./public');
-	//, { 
-	//	cache: false
-	//	});
-	
-	request.addListener('end', function() {
-		file.serve(request, response);
-	}); // client is done and ready to do something
-})
-server.listen(8000);
+var LiveStats = require("./lib/livestats");
+
+new LiveStats({
+	port: 8000,
+	geoipServer: {
+		hostname: 'geoip.peepcode.com'
+	, port: 80
+	}
+});
 
