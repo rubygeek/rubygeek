b10b81acd1e48c17d226b55961a687a312ee68cb
rubygeek <nola@rubygeek.com>
2011-07-02 18:01:02 -0500

    added markdown gem and helper method

--- ruby/trying_markdown_redcarpet/app/helpers/application_helper.rb
+++ ruby/trying_markdown_redcarpet/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :fenced_code, :gh_blockcode]
+    raw Redcarpet.new(text, *options).to_html.html_safe
+  end
 end
