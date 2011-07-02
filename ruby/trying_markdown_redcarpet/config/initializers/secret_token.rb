209fdcdebd744a3ca129841f42b5f637199502f5
rubygeek <nola@rubygeek.com>
2011-07-02 17:39:52 -0500

    added basic project and scaffold for articles

---
+++ ruby/trying_markdown_redcarpet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryingMarkdownRedcarpet::Application.config.secret_token = 'b35540aeefe65b82b4e52ae9ca95ba1538ec5603e5000a6ff8f4be54bbee8aa41356d6cde813b2d1ff379cdcba3353782da2a5bd87c63a5f6ed52e3b275f3e86'
