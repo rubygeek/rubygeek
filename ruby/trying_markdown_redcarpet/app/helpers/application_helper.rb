7233ba144ac320329c444417069008d358e640b2
rubygeek <nola@rubygeek.com>
2011-07-02 19:28:46 -0500

    adding syntax highlighting

--- ruby/trying_markdown_redcarpet/app/helpers/application_helper.rb
+++ ruby/trying_markdown_redcarpet/app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :fenced_code, :gh_blockcode]
-    raw Redcarpet.new(text, *options).to_html.html_safe
+    syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+  end
+  
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
   end
 end
