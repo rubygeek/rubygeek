a9b9e1a7ef329b0a5e78bf55c922e585d01a318a
rubygeek <nola@rubygeek.com>
2011-07-05 18:47:39 -0500

    added basic geolookup to location railscast #273

---
+++ ruby/trying_geocoder/app/models/location.rb
@@ -0,0 +1,6 @@
+class Location < ActiveRecord::Base
+  attr_accessible :address, :latitude, :longitude
+  
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+end
---
+++ ruby/trying_geocoder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryingGeocoder::Application.config.secret_token = '0dfcad262208c89e61aaecc9b790b5330b08345ff4d5025edd77bde24f573ac6985580d14e44e965570dd5b55c11d52adb2c9265c1419fadf0bc050e72a72783'
