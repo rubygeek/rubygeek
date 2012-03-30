20f9740b41b40e3e00f7e778c02c1f63fcae4e6c
rubygeek <nola@rubygeek.com>
2012-03-30 12:08:30 -0500

    added my coworkers solutions

--- ruby/trying_ruby/how_many_1s_in_120.rb
+++ ruby/trying_ruby/how_many_1s_in_120.rb
@@ -1,6 +1,10 @@
 # One day, a little boy who was very curious about numbers thought to himself, "If were to write out all the numbers from 1 to 120 
 # how many times would I write the numeral 1?"
 
+# I talked about this with my coworkers and they came up with these solutions:
+# Hal:   str = ""; 1.upto(120) {|i| str << i.to_s }; puts str.split("").count("1")
+# Ethan: (1..120).inject(0){|t,i| t+= i.to_s.count("1"); t}
+
 def how_many(max, value = 1)
   total = 0
   (1..max).each do |number|
