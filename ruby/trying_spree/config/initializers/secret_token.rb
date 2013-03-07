365220e1cde564c529972b5ae4f1d38e00db9934
rubygeek <nola@rubygeek.com>
2013-03-06 19:08:07 -0600

    trying out spree commerce

---
+++ ruby/trying_spree/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryingSpree::Application.config.secret_token = 'b5388aa00fcb49541d456f2d489225adc29f4e84db4da2d25548d6b355c192498f8d05af978b05b97270a66e5ed2ef9da6af81713dac1d08906ee8c2a9c63ea3'
