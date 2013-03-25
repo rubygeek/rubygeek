23511e0fb547062e4fe95d8027ad1ec654e69984
rubygeek <nola@rubygeek.com>
2013-03-25 08:04:51 -0500

    slicing strings

---
+++ python/basic/strings.py
@@ -0,0 +1,12 @@
+
+# length of string
+a = "hello world"
+length = len(a)
+print "Length of 'hello world is "
+print length
+print "index  : 01234567890"
+print "word is: " + a
+print "slice 0:2 is " + a[0:2]
+print "slice 0:5 is " + a[0:5]
+print "slice 6: is " + a[6:]
+print "slice :5 is " + a[:5]
