912eda7818cf68c4012c4894300a3a30bc9ca3dc
rubygeek <nola@rubygeek.com>
2014-08-09 10:35:39 -0500

    my server after episode 3, minus the file handling

--- nodejs/latn/server.js
+++ nodejs/latn/server.js
@@ -2,10 +2,16 @@
 var http = require('http');
 
 var handler = function (req, res) {
+  console.log('you want to go to: ' + req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  if (req.url == '/') {
+    res.end('Hello World\n');
+  } else if (req.url == '/bye') {
+    res.end('Goodbye World\n');
+  } else {
+    res.end('Whatcho talking about willis?!\n');
   }
-
+}
 var server = http.createServer(handler);
 
 server.listen(1337, '127.0.0.1');
