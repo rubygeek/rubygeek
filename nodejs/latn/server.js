1442ceeef47461fb7999fa010346e371643941b6
rubygeek <nola@rubygeek.com>
2014-08-09 14:02:00 -0500

    added package.json used express to make simple page

--- nodejs/latn/server.js
+++ nodejs/latn/server.js
@@ -1,22 +1,16 @@
-// from nodejs.org website
-var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env['PORT'] || 3000;
 
-var handler = function (req, res) {
-  console.log('you want to go to: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  if (req.url == '/') {
-    res.end('Hello World\n');
-  } else if (req.url == '/bye') {
-    res.end('Goodbye World\n');
-  } else {
-    res.end('Whatcho talking about willis?!\n');
-  }
-}
-var server = http.createServer(handler);
+app.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.send('<html><head><title>NodeSlash</title></head><body><h1>NodeSlash</h1></body></html>');
+});
 
-server.listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
 
+app.listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
 
