f1a7328ba163307497b6770e0b7263498373125a
rubygeek <nola@rubygeek.com>
2016-05-27 20:26:39 -0500

    wrote recursive reverse with helper function and with loop

--- clojure/samples/src/samples/recursion.clj
+++ clojure/samples/src/samples/recursion.clj
@@ -24,3 +24,27 @@
   (tail-length-helper ls 0))
 
 (tail-length [1 2 3])
+
+;; recursive reverse
+;; base case  = return acc when coll is only 1 in length
+;; advancing = take butlast 
+;; initializing = call helper with all but last and last added to acc
+
+(defn rrev-helper [coll acc]
+  (if (= 1 (count coll))
+    (conj acc (first coll))
+    (recur (butlast coll) (conj acc (last coll)))))
+
+(defn rrev [coll]
+  (rrev-helper coll []))
+
+;; now with loop
+(defn loop-rrev [coll]
+  (loop [coll coll acc []]
+    (if (= 1 (count coll))
+      (conj acc (first coll))
+      (recur (butlast coll) (conj acc (last coll))))))
+
+(loop-rrev [2 3 4 5 6])
+
+
--- clojure/samples/test/samples/recursion_test.clj
+++ clojure/samples/test/samples/recursion_test.clj
@@ -2,11 +2,14 @@
   (:require [clojure.test :refer :all]
             [samples.recursion :refer :all]))
 
-(deftest test-hello
-  (is (= "hello there nola" (hello "nola"))))
-
 (deftest test-length
   (is (= 3 (length [1 2 3]))))
 
 (deftest test-tail-length
   (is (= 3 (tail-length [1 2 3]))))
+
+(deftest test-recursive-reverse
+  (is (= [4 3 2 1] (rrev [1 2 3 4]))))
+
+(deftest test-loop-recursive-reverse
+  (is (= [4 3 2 1] (loop-rrev [1 2 3 4]))))
