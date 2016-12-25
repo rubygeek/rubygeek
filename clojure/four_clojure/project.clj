15b818aa80d01a8c01c9feec41d066cf6c415158
Nola Stowe <nola@rubygeek.com>
2016-12-25 17:00:37 -0600

    removed dirctory not used

--- clojure/four_clojure/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject four_clojure "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
-  :license {:name "TODO: Choose a license"
-            :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
-                   :source-paths ["dev"]}})
--- clojure/four_clojure/src/four_clojure/thirty_four.clj
+++
@@ -1,8 +0,0 @@
-(ns four-clojure.thirty-four)
-
-(defn d [start end]
-  (loop [end start]
-    (when (< end start)
-      (println start)
-      (recur (inc start)))))
-
