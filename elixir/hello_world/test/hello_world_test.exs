799b8ae7068a4287d47e9468ae35a95987d8ec17
rubygeek <nola@rubygeek.com>
2017-02-15 15:11:54 -0600

    fixed typo

--- elixir/hello_world/test/hello_world_test.exs
+++ elixir/hello_world/test/hello_world_test.exs
@@ -2,7 +2,7 @@ defmodule HelloWorldTest do
   use ExUnit.Case
   doctest HelloWorld
 
-  test "hello returns hellow" do
+  test "hello returns hello" do
     assert HelloWorld.hello == :world
   end
 end
