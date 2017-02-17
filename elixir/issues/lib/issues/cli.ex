f78944d4500a8fbad2c9ab1efa7473123ffce7de
rubygeek <nola@rubygeek.com>
2017-02-17 14:11:53 -0600

    updated to take only the number we need

--- elixir/issues/lib/issues/cli.ex
+++ elixir/issues/lib/issues/cli.ex
@@ -20,10 +20,11 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> sort_ascending
+    |> Enum.take(count)
   end
 
   def decode_response({:ok, body}), do: body
