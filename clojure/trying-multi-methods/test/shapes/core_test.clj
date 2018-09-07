b3c24e9c027efc0c1aa044ac55b267e06825c6dc
Nola Stowe <nola@rubygeek.com>
2018-09-07 08:52:40 -0500

    fixed spacing

--- clojure/trying-multi-methods/test/shapes/core_test.clj
+++ clojure/trying-multi-methods/test/shapes/core_test.clj
@@ -3,12 +3,10 @@
             [shapes.core :as shape]))
 
  (deftest helper-functions-test
-  (testing "building shapes"
-    (is (= :Circle (:Shape (shape/build-circle 10))))
-    (is (= :Triangle (:Shape (shape/build-triangle 10 5))))
-    (is (= :Rect (:Shape (shape/build-rect 10 5))))
-   ))
-
+   (testing "building shapes"
+     (is (= :Circle   (:Shape (shape/build-circle 10))))
+     (is (= :Triangle (:Shape (shape/build-triangle 10 5))))
+     (is (= :Rect     (:Shape (shape/build-rect 10 5))))))
 
  (deftest area-test
    (testing "calulating area"
@@ -16,7 +14,7 @@
            my-triangle (shape/build-triangle 5 10)
            my-rect (shape/build-rect 20 5)]
        (is (= 314.1592653589793 (shape/area my-circle)))
-       (is (= 25.0  (shape/area my-triangle)))
-       (is (= 100.0 (shape/area my-rect))))))
+       (is (= 25.0              (shape/area my-triangle)))
+       (is (= 100.0             (shape/area my-rect))))))
 
 
