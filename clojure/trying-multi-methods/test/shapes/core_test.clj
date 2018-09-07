3307cc7449ba7a73ea0ce951fb1033f7976bdef1
Nola Stowe <nola@rubygeek.com>
2018-09-07 08:58:38 -0500

    fixed typo, spacing

--- clojure/trying-multi-methods/test/shapes/core_test.clj
+++ clojure/trying-multi-methods/test/shapes/core_test.clj
@@ -9,10 +9,10 @@
      (is (= :Rect     (:Shape (shape/build-rect 10 5))))))
 
  (deftest area-test
-   (testing "calulating area"
-     (let [my-circle (shape/build-circle 10)
+   (testing "calculating area"
+     (let [my-circle   (shape/build-circle 10)
            my-triangle (shape/build-triangle 5 10)
-           my-rect (shape/build-rect 20 5)]
+           my-rect     (shape/build-rect 20 5)]
        (is (= 314.1592653589793 (shape/area my-circle)))
        (is (= 25.0              (shape/area my-triangle)))
        (is (= 100.0             (shape/area my-rect))))))
