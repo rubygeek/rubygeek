b7f5de8990aad522061184b23ab875328bf3296c
rubygeek <nola@rubygeek.com>
2018-09-30 12:12:24 -0500

    initial app

---
+++ javascript/trying-react-enzyme/src/components/scoreboard.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from "react";
+
+export default class Scoreboard extends React.Component {
+    render() {
+        return (
+		<div>
+		  <ul>
+                   <li> Score 1 </li>
+		   <li> Score 2 </li>
+	          </ul>
+                </div>);
+    };
+
+}
