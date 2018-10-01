28aea7160bccf373f8c6194ea96a336f2df2d492
rubygeek <nola@rubygeek.com>
2018-10-01 15:27:20 -0500

    removed old code

--- javascript/trying-react-enzyme/src/components/scoreboard.test.js
+++ javascript/trying-react-enzyme/src/components/scoreboard.test.js
@@ -1,15 +1,3 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-*/
-import React from 'react';
 import { shallow } from 'enzyme';
 import Scoreboard from "./scoreboard"
 
