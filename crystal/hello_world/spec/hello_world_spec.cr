227366f22aa898fc62199d7e0ee6c39723d0a4e3
Nola Stowe <nola@rubygeek.com>
2020-02-05 19:41:00 -0600

    added first crystal program

---
+++ crystal/hello_world/spec/hello_world_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe HelloWorld do
+  # TODO: Write tests
+
+  it "works" do
+    HelloWorld.say_hello().should eq("Hello World!")
+  end
+end
---
+++ crystal/hello_world/src/hello_world.cr
@@ -0,0 +1,9 @@
+# TODO: Write documentation for `HelloWorld`
+module HelloWorld
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+  def self.say_hello()
+     return "Hello World!"
+  end
+end
